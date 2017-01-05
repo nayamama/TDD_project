0262105b36c9127fb01d478e1d979599c1aa9e8e
qi <qi@qi-pydev.sqroot.local>
2017-01-04 22:16:24 -0500

    adjust models so that items are associated with different lists

--- lists/models.py
+++ lists/models.py
@@ -3,5 +3,10 @@ from django.db import models
 # Create your models here.
 # when I cannot migrate model after deleting existing table
 # http://stackoverflow.com/questions/29888046/django-1-8-create-initial-migrations-for-existing-schema
+class List(models.Model):
+	pass
+
 class Item(models.Model):
 	text = models.TextField(default="")
+	list = models.ForeignKey(List, default=None)
+
