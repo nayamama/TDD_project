7e218f94b32d236c3d5b99abb8edf0447baffd3c
qi <qi@qi-pydev.sqroot.local>
2017-01-08 17:48:54 -0500

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -26,7 +26,6 @@ class NewVistorTest(LiveServerTestCase):
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
-		# self.fail('Finish the test')
 
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
@@ -42,8 +41,8 @@ class NewVistorTest(LiveServerTestCase):
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
-		#import time
-		#time.sleep(10)
+		import time
+		time.sleep(5)
 
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
@@ -56,6 +55,10 @@ class NewVistorTest(LiveServerTestCase):
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
 
+		import time
+		time.sleep(5)
+
+
 		# The page updates again, and now shows both items on her list
 		#table = self.browser.find_element_by_id('id_list_table')
 		#rows = table.find_elements_by_tag_name('tr')
@@ -63,8 +66,6 @@ class NewVistorTest(LiveServerTestCase):
 		#self.assertIn('2: Use peacock feathers to make a fly' ,[row.text for row in rows])
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-		#self.fail('Finish the test')
 
 		# The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
--- lists/tests.py
+++ lists/tests.py
@@ -18,45 +18,6 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
-
-		#self.assertTrue(response.content.startswith(b'<html>'))
-		#self.assertIn(b'<title>To-Do lists</title>', response.content)
-		#self.assertTrue(response.content.strip().endswith(b'</html>'))
-
-	#def test_home_page_can_save_a_POST_request(self):
-	#	request = HttpRequest()
-	#	request.method = "POST"
-	#	request.POST["item_text"] = "A new list item"
-	#	response = home_page(request)	
-	#	self.assertEqual(Item.objects.count(), 1)
-	#	new_item = Item.objects.first()
-	#	self.assertEqual(new_item.text, "A new list item")
-		#self.assertIn("A new list item", response.content.decode())
-		#expected_html = render_to_string('home.html', {"new_item_text": "A new list item"})
-		#self.assertEqual(response.content.decode(), expected_html)
-		#self.assertEqual(response.status_code, 302)
-		#self.assertEqual(response['location'], '/')
-
-	#def test_home_page_redirects_after_POST(self):
-	#	request = HttpRequest()
-	#	request.method = "POST"
-	#	request.POST["item_text"] = "A new list item"
-	#	response = home_page(request)
-	#	self.assertEqual(response.status_code, 302)
-	#	self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-	#def test_home_page_only_saves_items_when_necessary(self):
-	#	request = HttpRequest()
-	#	home_page(request)
-	#	self.assertEqual(Item.objects.count(), 0)
-
-	#def test_home_page_displays_all_list_items(self):
-	#	Item.objects.create(text='item1')
-	#	Item.objects.create(text='item2')
-        #	request = HttpRequest()
-	#	response = home_page(request)
-	#	self.assertIn('item1', response.content.decode())
-	#	self.assertIn('item2', response.content.decode())
 	
 class ListAndItemModelsTest(TestCase):
 
@@ -89,19 +50,30 @@ class ListAndItemModelsTest(TestCase):
 class ListViewTest(TestCase):
 	
 	def test_uses_list_template(self):
-
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+		list_ = List.objects.create()
+		response = self.client.get('/lists/%d/' % (list_.id,))
 		self.assertTemplateUsed(response, 'list.html')
 	
-	def test_displays_all_items(self):
-		list_ = List.objects.create()
-		Item.objects.create(text='itemey 1', list = list_)
-		Item.objects.create(text='itemey 2', list = list_)
+	def test_displays_only_items_for_that_list(self):
+		correct_list = List.objects.create()
+		Item.objects.create(text='itemey 1', list=correct_list)
+		Item.objects.create(text='itemey 2', list=correct_list)
+		other_list = List.objects.create()
+		Item.objects.create(text='other list item 1', list=other_list)
+		Item.objects.create(text='other list item 2', list=other_list)
 
-		response = self.client.get('/lists/the-only-list-in-the-world/')
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
+		self.assertNotContains(response, 'other list item 1')
+		self.assertNotContains(response, 'other list item 2')
+
+	def test_passes_correct_list_to_template(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
+		self.assertEqual(response.context['list'], correct_list)
 
 class NewListTest(TestCase):
 
@@ -109,7 +81,6 @@ class NewListTest(TestCase):
 #		request = HttpRequest()
 #		request.method = "POST"
 #		request.POST["item_text"] = "A new list item"
-
 #		response = home_page(request)
 
 		self.client.post('/lists/new', data={'item_text': 'A new list item'})
@@ -122,11 +93,34 @@ class NewListTest(TestCase):
 #		request = HttpRequest()
 #		request.method = "POST"
 #		request.POST["item_text"] = "A new list item"
-
 #		response = home_page(request)
 
 		response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
 #		self.assertEqual(response.status_code, 302)
 #		self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		new_list = List.objects.first()
+		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+class NewItemTest(TestCase):
+
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text': 'A new item for an existing list'})
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.list, correct_list)
+
+	def test_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+
+		response = self.client.post('/lists/%d/add_item' % (correct_list.id,), data={'item_text': 'A new item for an existing list'})
+
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+		
