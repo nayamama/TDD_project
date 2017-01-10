8f1268e39604850323810ae522b6464064c22064
qi <qi@qi-pydev.sqroot.local>
2017-01-10 15:44:44 -0500

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -78,8 +78,7 @@ class NewVistorTest(LiveServerTestCase):
 		self.browser.quit()
 		self.browser = webdriver.Firefox()
 
-		# Francis visits the home page. There is no sign of Edith's
-		# list
+		# Francis visits the home page. There is no sign of Edith's list
 		self.browser.get(self.live_server_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
@@ -100,6 +99,20 @@ class NewVistorTest(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
+
+	def test_layout_and_styling(self):
+		# Edith goes to the homw page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+
+		# She noticed the input box is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
+
+		# She starts a new list and sees the input is nicely centered there too
+		inputbox.send_keys('testing\n')		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(inputbox.location['x'] + inputbox.size['width']/2, 512, delta=5)
 
 # Satisfied, they both go back to sleep
 
